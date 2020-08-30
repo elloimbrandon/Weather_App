c56f4920b8d3c42c41a438b2a4a2b213a38e505e
Brandon Feltz <elloimbrandon@gmail.com>
2020-08-29 17:55:06 -0700

    .

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,18 @@
 const request = require('postman-request')
 
+
+
 const forcast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=60a44bd8c2c3ad047e9d2b33954231aa&query=' + latitude + ',' + longitude + '&units=f'
     request({url, json: true }, (error, response) => { 
-    const {error: responseError, weather_descriptions, temperature, feelslike} = response.body.current
+    const {error: responseError, weather_descriptions, temperature, feelslike, humidity} = response.body.current
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (responseError) {
             callback('Unable to find location!', undefined)
         } else {
             callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out.' + 
-            ' It feels like ' + feelslike + ' degrees out.')
+            ' It feels like ' + feelslike + ' degrees out.' + ' With a humidity of ' + humidity + '%.')
         }
     })
 }
